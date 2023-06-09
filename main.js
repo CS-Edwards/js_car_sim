cbd2347945ab6ac7e95092475fd68d6de74c0d53
CS-Edwards <candaces.edwards@gmail.com>
2023-06-09 11:41:44 -1000

    completed Lec 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width *0.9);
-const car = new Car(road.getLaneCenter(2),100,30,50);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 
 //car.draw(ctx)
 
@@ -15,6 +15,7 @@ function animate(){
 
     canvas.height = window.innerHeight;
 
+    //illusion of movement
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.7-car.height);
 
--- road.js
+++ road.js
@@ -13,10 +13,23 @@ class Road{
         this.bottom = infinity;
 
 
+        const topLeft={x:this.left,y:this.top};
+        const topRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
+
+        this.borders=[
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ];
+
+
     }
 
     getLaneCenter(laneIndex){
         const laneWidth = this.width/this.laneCount;
+
+        //right most possible lane
         return this.left +laneWidth/2 +
             Math.min(laneIndex,this.laneCount-1)*laneWidth;
     }
@@ -25,25 +38,35 @@ class Road{
         ctx.lineWidth = 5;
         ctx.strokeStyle="black";
 
-        for(let i=0; i<=this.laneCount; i++){
+        for(let i=1; i<=this.laneCount-1; i++){
             const x=lerp(
                 this.left,
                 this.right,
                 i/this.laneCount
             )
 
-            if(i>0 && i<this.laneCount){
-                ctx.setLineDash([20,20]);
-            } else{
-                ctx.setLineDash([0,0]);
-            }
-        
+            
 
+            // if(i>0 && i<this.laneCount){
+            //     ctx.setLineDash([20,20]);
+            // } else{
+            //     ctx.setLineDash([0,0]);
+            // }
+        
+            ctx.setLineDash([20,20]);
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
             ctx.stroke(); 
         }
+
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        })
 
         // ctx.beginPath();
         // ctx.moveTo(this.right,this.top);
